4744c7d29d659b733375f32c07227f7f8e9aee7c
Asi-Asi <asi-ahmad@outlook.com>
2025-09-24 02:11:17 +0300

    ex router - fix 1

--- progress-pulse-server/router.js
+++ progress-pulse-server/router.js
@@ -2,6 +2,7 @@ import {Router} from 'express';
 import usersRouter from './services/users/users.routes.js';
 import authRoutes from './services/auth/auth.routes.js';
 import plansRouter from './services/plans/plans.routes.js';
+import exercisesRouter from './services/exercises/exercises.routes.js';
 
 const router = Router();
 
@@ -9,7 +10,7 @@ const router = Router();
 router.use('/users', usersRouter);  // => /api/users/*   (register)/(login) ....
 router.use('/auth', authRoutes);    // => /api/auth/*   (refresh/logout)
 router.use('/plans', plansRouter); // => /api/plans/*
-
+router.use('/exercises', exercisesRouter); // => /api/exercises/*
 
 
 
